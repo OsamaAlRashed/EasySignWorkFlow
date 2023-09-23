ee20bb052d7f50a91310ca7f7183e763ff0166b9
Osama Al-Rashed <osama.future19@gmail.com>
2023-09-23 12:56:12 +0400

    Refactoring

--- EasySignWorkFlow/Commons/ActionResult.cs
+++ EasySignWorkFlow/Commons/ActionResult.cs
@@ -5,6 +5,20 @@ namespace EasySignWorkFlow.Commons;
 public sealed class ActionResult<TStatus>
     where TStatus : struct, Enum
 {
+    public ActionResult(
+        ActionType actionType,
+        TStatus? previousStatus,
+        TStatus? newStatus,
+        bool isSucceeded,
+        string? message)
+    {
+        ActionType = actionType;
+        PreviousStatus = previousStatus;
+        NewStatus = newStatus;
+        IsSucceeded = isSucceeded;
+        Message = message;
+    }
+
     public ActionType ActionType { get; private set; }
     public TStatus? PreviousStatus { get; private set; }
     public TStatus? NewStatus { get; private set; }
@@ -13,28 +27,10 @@ public sealed class ActionResult<TStatus>
 
 
     internal static ActionResult<TStatus> SetSuccess(ActionType actionType, TStatus? previousStatus, TStatus? newStatus)
-    {
-        return new ActionResult<TStatus>()
-        {
-            IsSucceeded = true,
-            Message = string.Empty,
-            ActionType = actionType,
-            PreviousStatus = previousStatus,
-            NewStatus = newStatus,
-        };
-    }
+        => new(actionType, previousStatus, newStatus, true, string.Empty);
 
     internal static ActionResult<TStatus> SetFailed(ActionType actionType, TStatus? previousStatus, TStatus? newStatus, string message)
-    {
-        return new ActionResult<TStatus>()
-        {
-            IsSucceeded = false,
-            Message = message,
-            ActionType = actionType,
-            PreviousStatus = previousStatus,
-            NewStatus = newStatus,
-        };
-    }
+        => new(actionType, previousStatus, newStatus, false, message);
 
     public static bool operator true(ActionResult<TStatus> actionResult) => actionResult.IsSucceeded;
     public static bool operator false(ActionResult<TStatus> actionResult) => !actionResult.IsSucceeded;
