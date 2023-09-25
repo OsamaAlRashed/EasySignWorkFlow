4ab96816291cde82cc936b5fe1b392babade3315
Osama Al-Rashed <osama.future19@gmail.com>
2023-09-25 20:14:23 +0400

    Add New Tests

--- EasySignWorkFlow.Tests/ActionExtensionsTests.cs
+++ EasySignWorkFlow.Tests/ActionExtensionsTests.cs
@@ -20,16 +20,12 @@ public class ActionExtensionsTests
     }
 
     [Fact]
-    public void Test1()
+    public void Given_RequestIsDraft_When_Approve_Then_CurrentStatusIsWaitingForManager1()
     {
         // Arrange
         _flowMachine.When(MyRequestStatus.Draft)
             .If(request => request.Value == 0)
             .Set(MyRequestStatus.WaitingForManager1);
-
-        _flowMachine.When(MyRequestStatus.Draft)
-            .If(request => request.Value != 0)
-            .Set(MyRequestStatus.WaitingForManager2);
 
         // Act
         _request.OnCreate(_flowMachine);
@@ -40,18 +36,137 @@ public class ActionExtensionsTests
     }
 
     [Fact]
-    public void Test2()
+    public void Given_RequestIsDraft_When_ApproveTwice_Then_CurrentStatusIsAccepted()
     {
         // Arrange
         _flowMachine.When(MyRequestStatus.Draft)
-            .If(request => request.Value == 0)
             .Set(MyRequestStatus.WaitingForManager1);
+
+        _flowMachine.When(MyRequestStatus.WaitingForManager1)
+            .Set(MyRequestStatus.Accepted);
 
         // Act
         _request.OnCreate(_flowMachine);
         _request.Approve(_flowMachine);
+        _request.Approve(_flowMachine);
 
         //Assert
+        Assert.True(_request.CurrentStatus == MyRequestStatus.Accepted);
+    }
+
+    [Fact]
+    public void Given_RequestIsDraft_When_Approve3Times_Then_CurrentStatusIsAcceptedAndTheLastApproveWillFails()
+    {
+        // Arrange
+        _flowMachine.When(MyRequestStatus.Draft)
+            .Set(MyRequestStatus.WaitingForManager1);
+
+        _flowMachine.When(MyRequestStatus.WaitingForManager1)
+            .Set(MyRequestStatus.Accepted);
+
+        // Act
+        _request.OnCreate(_flowMachine);
+        _request.Approve(_flowMachine);
+        _request.Approve(_flowMachine);
+        var result = _request.Approve(_flowMachine);
+
+        //Assert
+        Assert.False(result);
+        Assert.True(_request.CurrentStatus == MyRequestStatus.Accepted);
+    }
+
+    [Fact]
+    public void Given_RequestIsDraft_When_ApproveWithoutCallOnCreate_Then_ApproveMethodThrowsAnException()
+    {
+        // Arrange
+        _flowMachine.When(MyRequestStatus.Draft)
+            .Set(MyRequestStatus.WaitingForManager1);
+
+        _flowMachine.When(MyRequestStatus.WaitingForManager1)
+            .Set(MyRequestStatus.Accepted);
+
+        // Act
+        
+        //Assert
+        Assert.ThrowsAny<Exception>(() => _request.Approve(_flowMachine));
+    }
+
+    [Fact]
+    public void Given_RequestIsDraft_When_Refuse_Then_CurrentStatusIsRefused()
+    {
+        // Arrange
+        _flowMachine.When(MyRequestStatus.Draft)
+            .Set(MyRequestStatus.WaitingForManager1);
+
+        _flowMachine.When(MyRequestStatus.WaitingForManager1)
+            .Set(MyRequestStatus.Accepted);
+
+        // Act
+        _request.OnCreate(_flowMachine);
+        _request.Approve(_flowMachine);
+        _request.Refuse(_flowMachine);
+
+        //Assert
+        Assert.True(_request.CurrentStatus == MyRequestStatus.Refused);
+    }
+
+    [Fact]
+    public void Given_RequestIsRefused_When_RefuseAgain_Then_RefuseWillFails()
+    {
+        // Arrange
+        _flowMachine.When(MyRequestStatus.Draft)
+            .Set(MyRequestStatus.WaitingForManager1);
+
+        _flowMachine.When(MyRequestStatus.WaitingForManager1)
+            .Set(MyRequestStatus.Accepted);
+
+        // Act
+        _request.OnCreate(_flowMachine);
+        _request.Approve(_flowMachine);
+        _request.Refuse(_flowMachine);
+        var result = _request.Refuse(_flowMachine);
+
+        //Assert
+        Assert.False(result);
+        Assert.True(_request.CurrentStatus == MyRequestStatus.Refused);
+    }
+
+    [Fact]
+    public void Given_RequestIsRefused_When_Reset_Then_CurrentStatusIsDraft()
+    {
+        // Arrange
+        _flowMachine.When(MyRequestStatus.Draft)
+            .Set(MyRequestStatus.WaitingForManager1);
+
+        _flowMachine.When(MyRequestStatus.WaitingForManager1)
+            .Set(MyRequestStatus.Accepted);
+
+        // Act
+        _request.OnCreate(_flowMachine);
+        _request.Refuse(_flowMachine);
+        _request.Reset(_flowMachine);
+
+        //Assert
+        Assert.True(_request.CurrentStatus == MyRequestStatus.Draft);
+    }
+
+    [Fact]
+    public void Given_RequestIsWaitingForManager1_When_Reset_Then_ResetWillFails()
+    {
+        // Arrange
+        _flowMachine.When(MyRequestStatus.Draft)
+            .Set(MyRequestStatus.WaitingForManager1);
+
+        _flowMachine.When(MyRequestStatus.WaitingForManager1)
+            .Set(MyRequestStatus.Accepted);
+
+        // Act
+        _request.OnCreate(_flowMachine);
+        _request.Approve(_flowMachine);
+        var result = _request.Reset(_flowMachine);
+
+        //Assert
+        Assert.False(result);
         Assert.True(_request.CurrentStatus == MyRequestStatus.WaitingForManager1);
     }
 }
